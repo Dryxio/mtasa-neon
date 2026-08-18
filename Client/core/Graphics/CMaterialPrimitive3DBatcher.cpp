5be10753bbe3b3db57dac904d4eb3972cc339a08
Dryxio <momemtumcorpfzco@gmail.com>
2026-08-18 17:08:52 +0100

    Queue raw GTA textures through material 3D batcher

--- Client/core/Graphics/CMaterialPrimitive3DBatcher.cpp
+++ Client/core/Graphics/CMaterialPrimitive3DBatcher.cpp
@@ -12,6 +12,7 @@
 #include <StdInc.h>
 #include "CMaterialPrimitive3DBatcher.h"
 #include "DXHook/CProxyDirect3DDevice9.h"
+#include "DXHook/CDirect3DEvents9.h"
 ////////////////////////////////////////////////////////////////
 //
 // CMaterialPrimitive3DBatcher::CMaterialPrimitive3DBatcher
@@ -50,18 +51,11 @@ void CMaterialPrimitive3DBatcher::Flush()
     if (m_primitiveList.empty())
         return;
 
-    // Get scene matrices
     D3DXMATRIX matWorld;
     D3DXMatrixIdentity(&matWorld);
     const D3DXMATRIX& matView = g_pDeviceState->TransformState.VIEW;
     const D3DXMATRIX& matProjection = g_pDeviceState->TransformState.PROJECTION;
 
-    // Pre-calc camera position
-    D3DXMATRIX matViewInv;
-    D3DXMatrixInverse(&matViewInv, NULL, &matView);
-    const CVector vecCameraPos(matViewInv._41, matViewInv._42, matViewInv._43);
-
-    // Set transforms
     m_pDevice->SetTransform(D3DTS_WORLD, &matWorld);
     m_pDevice->SetTransform(D3DTS_VIEW, &matView);
     m_pDevice->SetTransform(D3DTS_PROJECTION, &matProjection);
@@ -69,9 +63,6 @@ void CMaterialPrimitive3DBatcher::Flush()
     IDirect3DStateBlock9* pSavedStateBlock = nullptr;
     m_pDevice->CreateStateBlock(D3DSBT_ALL, &pSavedStateBlock);
 
-    // fix alpha blending, func at address 0x005D6480 drawing models in gtasa
-
-    // Set states
     if (g_pDeviceState->AdapterState.bRequiresClipping)
         m_pDevice->SetRenderState(D3DRS_CLIPPING, TRUE);
     m_pDevice->SetRenderState(D3DRS_ZENABLE, m_bPreGUI ? D3DZB_TRUE : D3DZB_FALSE);
@@ -95,29 +86,45 @@ void CMaterialPrimitive3DBatcher::Flush()
     m_pDevice->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_DIFFUSE);
     m_pDevice->SetTextureStageState(1, D3DTSS_COLOROP, D3DTOP_DISABLE);
     m_pDevice->SetTextureStageState(1, D3DTSS_ALPHAOP, D3DTOP_DISABLE);
-
-    // Set vertex stream
+    m_pDevice->SetSamplerState(0, D3DSAMP_ADDRESSU, D3DTADDRESS_WRAP);
+    m_pDevice->SetSamplerState(0, D3DSAMP_ADDRESSV, D3DTADDRESS_WRAP);
     m_pDevice->SetFVF(PrimitiveMaterialVertice::FNV);
 
-    // Draw
     m_pDevice->SetTexture(0, nullptr);
-    // Cache last used material, so we don't set directx parameters needlessly
     CMaterialItem* pLastMaterial = nullptr;
-
     uint uiVertexStreamZeroStride = sizeof(PrimitiveMaterialVertice);
 
     for (auto& primitive : m_primitiveList)
     {
-        const void* pVertexStreamZeroData = &primitive.pVecVertices->at(0);
-        size_t      iCollectionSize = primitive.pVecVertices->size();
+        if (!primitive.pVecVertices || primitive.pVecVertices->empty())
+            continue;
+
+        const void* pVertexStreamZeroData = primitive.pVecVertices->data();
+
+        if (primitive.pRawTexture)
+        {
+            // RenderWare textures are created through the D3D proxy. The queued
+            // batcher renders on the raw device, so unwrap the proxy first just
+            // like DrawTextureRaw does. This is the important difference from
+            // the old break-effect renderer which crashed inside d3d9.dll.
+            IDirect3DTexture9* pRealTexture = reinterpret_cast<IDirect3DTexture9*>(CDirect3DEvents9::GetRealTexture(primitive.pRawTexture));
+            if (pRealTexture)
+            {
+                m_pDevice->SetTexture(0, pRealTexture);
+                DrawPrimitive(primitive.eType, primitive.pVecVertices->size(), pVertexStreamZeroData, uiVertexStreamZeroStride);
+            }
+            pLastMaterial = nullptr;
+            continue;
+        }
 
         CMaterialItem* pMaterial = primitive.pMaterial;
+        if (!pMaterial)
+            continue;
+
         if (pMaterial != pLastMaterial)
         {
-            // Set texture addressing mode
             m_pDevice->SetSamplerState(0, D3DSAMP_ADDRESSU, pMaterial->m_TextureAddress);
             m_pDevice->SetSamplerState(0, D3DSAMP_ADDRESSV, pMaterial->m_TextureAddress);
-
             if (pMaterial->m_TextureAddress == TADDRESS_BORDER)
                 m_pDevice->SetSamplerState(0, D3DSAMP_BORDERCOLOR, pMaterial->m_uiBorderColor);
         }
@@ -143,24 +150,17 @@ void CMaterialPrimitive3DBatcher::Flush()
         }
         else if (CShaderInstance* pShaderInstance = DynamicCast<CShaderInstance>(pMaterial))
         {
-            // Draw using shader
             ID3DXEffect* pD3DEffect = pShaderInstance->m_pEffectWrap->m_pD3DEffect;
-
             if (pMaterial != pLastMaterial)
             {
-                // Apply custom parameters
                 pShaderInstance->ApplyShaderParameters();
-                // Apply common parameters
                 pShaderInstance->m_pEffectWrap->ApplyCommonHandles();
-                // Apply mapped parameters
                 pShaderInstance->m_pEffectWrap->ApplyMappedHandles();
             }
 
-            // Do shader passes
             DWORD dwFlags = D3DXFX_DONOTSAVESHADERSTATE;
-            uint  uiNumPasses = 0;
+            uint uiNumPasses = 0;
             pShaderInstance->m_pEffectWrap->Begin(&uiNumPasses, dwFlags, false);
-
             for (uint uiPass = 0; uiPass < uiNumPasses; uiPass++)
             {
                 pD3DEffect->BeginPass(uiPass);
@@ -168,20 +168,18 @@ void CMaterialPrimitive3DBatcher::Flush()
                 pD3DEffect->EndPass();
             }
             pShaderInstance->m_pEffectWrap->End();
-
-            // If we didn't get the effect to save the shader state, clear some things here
             if (dwFlags & D3DXFX_DONOTSAVESHADERSTATE)
             {
                 m_pDevice->SetVertexShader(NULL);
                 m_pDevice->SetPixelShader(NULL);
             }
         }
+
         pLastMaterial = pMaterial;
         pMaterial->Release();
         primitive.pMaterial = nullptr;
     }
 
-    // Clean up
     ClearQueue();
 
     if (pSavedStateBlock)
@@ -193,59 +191,78 @@ void CMaterialPrimitive3DBatcher::Flush()
 
 void CMaterialPrimitive3DBatcher::ClearQueue()
 {
-    // Clean up
     for (auto& primitive : m_primitiveList)
     {
         SAFE_RELEASE(primitive.pMaterial);
+        SAFE_RELEASE(primitive.pRawTexture);
         delete primitive.pVecVertices;
         primitive.pVecVertices = nullptr;
     }
-
     m_primitiveList.clear();
 }
-////////////////////////////////////////////////////////////////
-//
-// CPrimitiveMaterialBatcher::DrawPrimitive
-//
-// Draws the primitives on render target
-//
-////////////////////////////////////////////////////////////////
+
 void CMaterialPrimitive3DBatcher::DrawPrimitive(D3DPRIMITIVETYPE eType, size_t iCollectionSize, const void* pDataAddr, size_t uiVertexStride)
 {
     int iSize = 1;
     switch (eType)
     {
-        case D3DPT_POINTLIST:
-            iSize = iCollectionSize;
-            break;
-        case D3DPT_LINELIST:
-            iSize = iCollectionSize / 2;
-            break;
-        case D3DPT_LINESTRIP:
-            iSize = iCollectionSize - 1;
-            break;
+        case D3DPT_POINTLIST: iSize = iCollectionSize; break;
+        case D3DPT_LINELIST: iSize = iCollectionSize / 2; break;
+        case D3DPT_LINESTRIP: iSize = iCollectionSize - 1; break;
         case D3DPT_TRIANGLEFAN:
-        case D3DPT_TRIANGLESTRIP:
-            iSize = iCollectionSize - 2;
-            break;
-        case D3DPT_TRIANGLELIST:
-            iSize = iCollectionSize / 3;
-            break;
+        case D3DPT_TRIANGLESTRIP: iSize = iCollectionSize - 2; break;
+        case D3DPT_TRIANGLELIST: iSize = iCollectionSize / 3; break;
     }
     m_pDevice->DrawPrimitiveUP(eType, iSize, pDataAddr, uiVertexStride);
 }
-////////////////////////////////////////////////////////////////
-//
-// CMaterialPrimitive3DBatcher::AddPrimitive
-//
-// Add a new primitive to the list
-//
-////////////////////////////////////////////////////////////////
+
 void CMaterialPrimitive3DBatcher::AddPrimitive(D3DPRIMITIVETYPE eType, CMaterialItem* pMaterial, std::vector<PrimitiveMaterialVertice>* pVecVertices)
 {
-    if (!pMaterial)
+    if (!pMaterial || !pVecVertices)
+    {
+        delete pVecVertices;
         return;
+    }
 
     pMaterial->AddRef();
-    m_primitiveList.push_back({eType, pMaterial, pVecVertices});
+    m_primitiveList.push_back({eType, pMaterial, nullptr, pVecVertices});
+}
+
+void CMaterialPrimitive3DBatcher::AddRawPrimitive(D3DPRIMITIVETYPE eType, IDirect3DTexture9* pTexture,
+                                                   std::vector<PrimitiveMaterialVertice>* pVecVertices)
+{
+    if (!pTexture || !pVecVertices)
+    {
+        delete pVecVertices;
+        return;
+    }
+
+    pTexture->AddRef();
+    m_primitiveList.push_back({eType, nullptr, pTexture, pVecVertices});
+}
+
+void CGraphics::DrawRawMaterialPrimitive3DQueued(std::vector<PrimitiveMaterialVertice>* pVecVertices, D3DPRIMITIVETYPE eType,
+                                                  IDirect3DTexture9* pTexture, eRenderStage stage)
+{
+    if (!pVecVertices)
+        return;
+    if (g_pCore->IsWindowMinimized() || !pTexture)
+    {
+        delete pVecVertices;
+        return;
+    }
+
+    switch (stage)
+    {
+        case eRenderStage::POST_GUI:
+            m_pMaterialPrimitive3DBatcherPostGUI->AddRawPrimitive(eType, pTexture, pVecVertices);
+            break;
+        case eRenderStage::POST_FX:
+            m_pMaterialPrimitive3DBatcherPostFX->AddRawPrimitive(eType, pTexture, pVecVertices);
+            break;
+        case eRenderStage::PRE_FX:
+        default:
+            m_pMaterialPrimitive3DBatcherPreGUI->AddRawPrimitive(eType, pTexture, pVecVertices);
+            break;
+    }
 }
